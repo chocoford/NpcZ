2af8d969df9ad3ef6588dd381470a6a38b2116d9
Chocoford <504001583@qq.com>
2021-04-20 16:15:31 +0800

    Initial Commit

---
+++ NPC-Z/NPC_ZApp.swift
@@ -0,0 +1,17 @@
+//
+//  NPC_ZApp.swift
+//  NPC-Z
+//
+//  Created by Chocoford on 2021/4/20.
+//
+
+import SwiftUI
+
+@main
+struct NPC_ZApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
